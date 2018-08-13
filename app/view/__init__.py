5f21f855111ec252bb92f757f4daee444328d7a9
Maydev <alice.maydev@gmail.com>
2018-08-13 17:08:53 +0900

    Update attrcallback and Router

--- app/view/__init__.py
+++ app/view/__init__.py
@@ -1,31 +1,30 @@
 import glob
 from os.path import dirname, basename, isfile
+from typing import Callable
+from collections import defaultdict
 
 from app import app
+from app.lib.attrcallback import AttrCallback
 
 class Router:
-    class REST:
-        def __init__(self, routes):
-            self.routes = routes
-            self.url = None
-
-        def __setattr__(self, method, handler):
-            if method in ['get', 'post', 'put', 'delete']:
-                self.routes[self.url] = (method, handler)
-            else:
-                super(Router.REST, self).__setattr__(method, handler)
+    routers = list()
 
     def __init__(self, route: str = ''):
-        self.routes = dict()
-        self.setter = Router.REST(self.routes)
+        self.routes = defaultdict(dict)
+        self.url = None
+        self.attr = AttrCallback(self.setter, ['get', 'post', 'put', 'delete'])
+        Router.routers.append((route, self.routes))
 
-    def route(self, url):
-        self.setter.url = url
-        return self.setter
+    def setter(self, method: str, handler: Callable) -> None:
+        self.routes[self.url][method] = handler
 
+    def route(self, url) -> AttrCallback:
+        self.url = url
+        return self.attr
+
+# import submodule of view
 files = glob.glob(dirname(__file__) + "/*.py")
-modules = map(basename, filter(lambda f: isfile(f) and not f.startswith('__'), files))
-__all__ = list(map(lambda f: f.split('.')[0], modules))
+__all__ = [basename(f).split('.')[0] for f in files if isfile(f) and not f.startswith('__')]
 
 @app.route('/')
 def index():
