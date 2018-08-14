3e8b852e82ff779cc723fb43cdcc7ec1416954f9
Maydev <alice.maydev@gmail.com>
2018-08-14 12:56:43 +0900

    Remove legacy utils

--- app/lib/utils.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def get_assignments():
-    df = pd.read_csv('./data/assignments.csv')
-    return [{k:v for k, v in zip(df.columns, val)} for val in df.values]
-
-def get_assignment(id):
-    df = pd.read_csv('./data/assignments.csv')
-    return dict(df.iloc[id])
