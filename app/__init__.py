a61979d2a1d83f9170bb262ea7926e953d614f62
Maydev <alice.maydev@gmail.com>
2018-08-14 15:53:12 +0900

    Update default app

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,26 @@
+import json
+
 from flask import Flask
+from flask_login import LoginManager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
+with open('config.json') as f:
+    conf = json.load(f)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 # app config
-app.secret_key = 'SECRET_KEY'
+app.secret_key = conf['APP']['secret_key']
 
 # db
-Engine      = create_engine('mysql+pymysql://root@localhost/test?charset=utf8', convert_unicode=True)
+Engine      = create_engine('mysql+pymysql://{}@{}/{}?charset=utf8'.format(
+                            conf['DB']['user'],
+                            conf['DB']['server'],
+                            conf['DB']['db']), convert_unicode=True)
 Session     = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=Engine))
