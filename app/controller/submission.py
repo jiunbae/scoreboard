337f6cfa09a69d6af23479fa5afa7b321d015832
Maydev <alice.maydev@gmail.com>
2018-08-14 18:33:46 +0900

    Update controller.Submission write_file

--- app/controller/submission.py
+++ app/controller/submission.py
@@ -1,5 +1,16 @@
+from os.path import join
+
+from werkzeug.utils import secure_filename
+
+from app import app
 from app.controller import Controller
 from app.models import Submission as submission
 
 class Submission(Controller):
     model = submission
+
+    @classmethod
+    def write_file(cls, file) -> None:
+        filename = secure_filename(file.filename)
+        file.save(join(app.config['UPLOAD_FOLDER'], filename))
+        return filename
