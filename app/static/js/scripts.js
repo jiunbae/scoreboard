1659042435ac9e3f325cefa40b382a306c19dbac
Maydev <alice.maydev@gmail.com>
2018-08-14 15:52:54 +0900

    Update statics

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   // auto description
   $('#upload[type="file"]').change((e) => {
-    var target = $('#description');
+    var target = $('input[name="description"]');
     var description = new Date().toISOString().slice(0, 10)+' '+e.target.files[0].name;
     if (target.val() == '') {
       target.val(description);
