3a18341cad783b2fe029dc25f745e8a8e9ef501f
Maydev <alice.maydev@gmail.com>
2018-08-20 00:43:47 +0900

    Update differ

--- app/lib/differ.py
+++ app/lib/differ.py
@@ -1,8 +1,30 @@
-class Differ:
-    def __init__(self, tar, obj):        
-        pass
+class Metric:
+    pass
 
-# Metric
-# - mIOU
-# - classification rate
-# - mAP
+class Classification(Metric):
+    @staticmethod
+    def clac(tar, obj):
+        return sum(all(trow.values[1:].argmax()+1 == orow.values[1:])
+                    for (_, trow), (_, orow) in zip(tar.iterrows(), obj.iterrows()))
+
+class MIOU(Metric):
+    pass
+
+class MAP(Metric):
+    pass
+
+import numpy as np
+import pandas as pd
+
+class Differ:
+    def __init__(self, tar: str, obj: str, metric: Metric):
+        self.tar = Differ.read(tar, index=None)
+        self.obj = Differ.read(obj, index=None)
+        self.metric = metric.calc
+
+    @staticmethod
+    def read(file: str):
+        return pd.read_csv(file)
+
+    def score(self) -> float:
+        return self.metric(self.tar, self.obj)
