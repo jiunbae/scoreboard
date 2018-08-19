83886817a1b4cb061e43fb283b786065bc18e764
bae jiun, maybe <alice.maydev@gmail.com>
2018-08-20 01:54:18 +0900

    Update user change password

--- app/controller/user.py
+++ app/controller/user.py
@@ -19,16 +19,34 @@ class User(Controller):
     @classmethod
     def login(cls, sid: int, pw: str) -> Optional[Base]:
         try:
-            instance = cls.model.query.filter_by(studentid=sid).filter_by(password=pw).one()
-            login_user(instance)
-            return instance
+            instance = cls.model.query.filter_by(studentid=sid).one()
+            if instance.assert_password(pw):
+                login_user(instance)
+                return instance
+            else:
+                # case password not matched
+                return None
         except:
+            # case not registered
             return None
 
     @classmethod
     @login_required
     def logout(cls) -> None:
         logout_user()
+
+    @classmethod
+    def update(cls, pw: str) -> bool:
+        instance = cls.current()
+        print (instance, pw)
+        if instance.assert_password(pw):
+            print (instance.password)
+            instance.set_safe_password(pw)
+            print (instance.password)
+            Session.add(instance)
+            Session.commit()
+            return instance
+        return None
 
 @login_manager.user_loader
 def load_user(uid):
