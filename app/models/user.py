76494ee8e6d1b73b55693df9ef78c6441915782d
bae jiun, maybe <alice.maydev@gmail.com>
2018-08-20 01:53:46 +0900

    Update user password with hash and salt

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,8 @@
 from app import Base
 
 from flask_login import UserMixin
-from sqlalchemy import Column, Integer, String
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 
 class User(Base, UserMixin):
@@ -9,13 +10,20 @@ class User(Base, UserMixin):
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
 
     id          = Column(Integer, primary_key=True, unique=True)
-    studentid   = Column(String(12))
-    password    = Column(String(32))
+    studentid   = Column(String(12), unique=True)
+    password    = Column(String(96))
+    TA          = Column(Boolean)
     submissions = relationship("Submission")
 
     def __init__(self, studentid, password):
         self.studentid = studentid
-        self.password = password
+        self.set_safe_password(password)
 
     def __repr__(self) -> str:
         return ','.join(map(str, [self.studentid, self.password]))
+
+    def set_safe_password(self, password: str):
+        self.password = generate_password_hash(password)
+
+    def assert_password(self, password: str) -> bool:
+        return check_password_hash(self.password, password)
