4a8628de9e4173b91f71b424346a4c660cb49f15
bae jiun, maybe <alice.maydev@gmail.com>
2018-08-21 09:23:49 +0900

    Update statics

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function() {
   // auto description
   $('#upload[type="file"]').change((e) => {
-    var target = $('input[name="description"]');
+    var target = $('input[name*="description"]');
     var description = new Date().toISOString().slice(0, 10)+' '+e.target.files[0].name;
     if (target.val() == '') {
       target.val(description);
@@ -18,5 +18,25 @@ $(document).ready(function(){
   $('#change').click((e) => {
     // request to user as put method
     // change password to new one
-  };
+  });
+
+  // dropdown
+  $('.dropdown-menu a.dropdown-item').click(function() {
+    var type = $('input[name*="assignmentType"]');
+    type.text($(this).text());
+    type.val($(this).text());
+  });
+
+  // datepicker
+  $('input[name*="date-from"]').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    format: 'yyyy-mm-dd'
+  });
+
+  $('input[name*="date-to"]').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    format: 'yyyy-mm-dd'
+  });
 });
